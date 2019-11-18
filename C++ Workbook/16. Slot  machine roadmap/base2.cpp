86c3fa096163d86a43060849e09d6040809b7046
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2019-11-18 11:20:27 -0800

    Day's Work

--- C++ Workbook/16. Slot  machine roadmap/base2.cpp
+++ C++ Workbook/16. Slot  machine roadmap/base2.cpp
@@ -1,17 +1,5 @@
 // base code file
 #include"Gaby.h"
-#include <iostream>
-#include <windows.h>
-#include <stdlib.h>
-#include <time.h>
-#include <conio.h>
-#include <sstream>
-#include <fstream>
-using namespace std;
-void gotoxy(short x, short y) {
-	COORD pos = {x, y};
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-	}
 // generates a random number between 0 and r inclusive
 int random(int r)
 {
@@ -22,6 +10,10 @@ main()
 {
   // write code here
   	double bet;
+  	int num1;
+  	int num2;
+  	int num3;
+  	char input;
   	cout<<"How much money will you bet:  $";
   	cin>>bet;
   	framedbox(0,0,20,50,'$');
@@ -29,7 +21,14 @@ main()
   	framedbox(4,8,5,7,'#');
   	framedbox(19,8,5,7,'#');
   	framedbox(34,8,5,7,'#');
-  	gotoxy(21,0);
-  	cout<<random(100);
+	while(kbhit() == 0)
+	{
+		gotoxy(6,10);
+		cout<<random(99);
+		gotoxy(21,10);
+		cout<<random(99);
+		gotoxy(36,10);
+		cout<<random(99);
+	}
 }
       
---
+++ Warm-Up/C++/3. Practice Loops/baseCode.cpp
@@ -0,0 +1,77 @@
+// base code file
+
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <sstream>
+#include <fstream>
+using namespace std;
+void gotoxy(short x, short y) {
+	COORD pos = {x, y};
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	}
+// generates a random number between 0 and r inclusive
+int random(int r)
+{
+    return rand()% r + 1;
+}  
+///////////////////////////////////////////////////////////////////////
+main()
+{
+      srand(time(NULL)); 
+  	// write code here
+	// leave the following line in all programs
+  	char c;
+  	char v;
+  	int counter = 1;
+  	int counter2 = 1;
+  	cout<<"What shall be the symbol for the C?  ";
+  	cin>>c;
+  	cout<<"What shall be the symbol for the V?  ";
+  	cin>>v;
+  	gotoxy(5,5);
+  	for(int i = 0; i < 10; i++)
+  	{
+  		for(int i = 0; i < 5; i++)
+  		{
+  			cout<<c;
+		}
+		gotoxy(5, 5 + counter);
+  		counter++;
+	}
+	counter = 1;
+	gotoxy(6,6);
+	for(int i = 0; i < 8; i++)
+  	{
+  		for(int i = 0; i < 4; i++)
+  		{
+  			cout<<' ';
+		}
+		gotoxy(6, 6 + counter);
+  		counter++;
+	}
+	gotoxy(15,5);
+	counter = 1;
+	for(int i = 0; i < 10; i++)
+	{
+		cout<<v;
+		gotoxy(15 + counter, 5 + counter2);
+		counter++;
+		counter2++;
+	}
+	counter = 1;
+	counter2 = 1;
+	gotoxy(24,14);
+	for(int i = 0; i < 10; i++)
+	{
+		cout<<v;
+		gotoxy(24 + counter, 14 - counter);
+		counter++;
+		counter2++;
+	}
+      getch();
+}
+
+
