c232c5169307099157a9106b7288dd5cbf527685
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2020-01-08 11:49:59 -0800

    Day's Work

---
+++ Python Labs/1. Python Intro/First.py
@@ -0,0 +1,5 @@
+#Lab 1
+print("Happy Birthday to you!")
+print("Happy Birthday to you!")
+print("Happy Birthday dear Monty!")
+print("Happy Birthday to you!")
---
+++ Python Labs/2. Python Concat/First.py
@@ -0,0 +1,6 @@
+#Lab 1
+line1 = "Happy Birthday to you,"
+line2 = "Happy Birthday dear "
+name = "Monty"
+print(line1)
+print(line1)
