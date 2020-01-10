b67730cd870e1b9d510ce2a15e8143e3f1f351c0
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2020-01-10 11:20:15 -0800

    Day's Work

---
+++ Python/Python Labs/10. Python Graphics/First.py
@@ -0,0 +1,7 @@
+#Lab 10
+from graphics import *
+window = GraphWin("Window",1000,1000)
+rectangle = Rectangle((10,10),(300,300))
+rectangle.draw(window)
+window.getMouse()
+window.close()
--- Python/Python Labs/9. Python nested loops/First.py
+++ Python/Python Labs/9. Python nested loops/First.py
@@ -1,1 +1,7 @@
 #Lab 9
+width = input("What word will be your width? ")
+length = input("What word will be your length? ")
+for x in width:
+    for y in length:
+        print(x,end='')
+    print()
