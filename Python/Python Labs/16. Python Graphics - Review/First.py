17650e697b05f182c2b5a495d28b3cd535ee54c9
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2020-01-22 11:49:17 -0800

    Day's Work

--- Python/Python Labs/16. Python Graphics - Review/First.py
+++ Python/Python Labs/16. Python Graphics - Review/First.py
@@ -1,9 +1,9 @@
 #Lab 12
 from graphics import *
-window = GraphWin('window',1000,1000)
 
 #Message
 tex = input("What message would you like to send? ")
+window = GraphWin('window',1000,1000)
 message = Text(Point(500,475),tex)
 message.setSize(36)
 message.setStyle("bold italic")
@@ -39,5 +39,88 @@ blue = Rectangle(Point(20,200),Point(40,220))
 blue.setFill('blue')
 blue.draw(window)
 
+red1 = Rectangle(Point(40,200),Point(70,203))
+red1.setFill('red')
+red1.draw(window)
+
+white1 = Rectangle(Point(40,203),Point(70,206))
+white1.setFill('white')
+white1.draw(window)
+
+red2 = Rectangle(Point(40,206),Point(70,209))
+red2.setFill('red')
+red2.draw(window)
+
+white1 = Rectangle(Point(40,209),Point(70,212))
+white1.setFill('white')
+white1.draw(window)
+
+red3 = Rectangle(Point(40,212),Point(70,215))
+red3.setFill('red')
+red3.draw(window)
+
+white1 = Rectangle(Point(40,215),Point(70,218))
+white1.setFill('white')
+white1.draw(window)
+
+red4 = Rectangle(Point(40,218),Point(70,220))
+red4.setFill('red')
+red4.draw(window)
+
+white1 = Rectangle(Point(20,220),Point(70,223))
+white1.setFill('white')
+white1.draw(window)
+
+red5 = Rectangle(Point(20,223),Point(70,226))
+red5.setFill('red')
+red5.draw(window)
+
+white1 = Rectangle(Point(20,226),Point(70,229))
+white1.setFill('white')
+white1.draw(window)
+
+red6 = Rectangle(Point(20,229),Point(70,232))
+red6.setFill('red')
+red6.draw(window)
+
+white1 = Rectangle(Point(20,232),Point(70,235))
+white1.setFill('white')
+white1.draw(window)
+
+red7 = Rectangle(Point(20,235),Point(70,238))
+red7.setFill('red')
+red7.draw(window)
+
 #Person
+
+pants = Polygon(Point(40,600),Point(50,600),Point(55,545),Point(60,600),Point(70,600),Point(65,550),Point(45,550))
+pants.setFill('black')
+pants.draw(window)
+
+shirt = Rectangle(Point(35,500),Point(65,550))
+shirt.setFill('red')
+shirt.draw(window)
+
+rightarm = Rectangle(Point(55,500),Point(85,510))
+rightarm.setFill('red')
+rightarm.draw(window)
+
+leftarm = Rectangle(Point(25,500),Point(55,510))
+leftarm.setFill('red')
+leftarm.draw(window)
+
+head = Circle(Point(55,480),20)
+head.setFill('tan')
+head.draw(window)
+
+mouth = Circle(Point(55,485),10)
+mouth.setFill('red')
+mouth.setOutline('red')
+mouth.draw(window)
+
+mouthcover = Rectangle(Point(45,475),Point(65.480))
+mouthcover.setFill('tan')
+mouthcover.setOutline('tan')
+mouthcover.draw(window)
+
 
--- Warm-Up/Python/3. Area/First.py
+++ Warm-Up/Python/3. Area/First.py
@@ -1,18 +1,27 @@
 #Lab 12
+import math
 shape = input("What shape would you like to find the area of? ")
+
+#Area of a Rectangle
 if(shape == "rectangle"):
     side1 = input("What is the length of side #1? ")
     side2 = input("What is the length of side #2? ")
     print("The area of your rectangle is: ", end='')
-    print(int(side1)*int(side2))
+    print(int(side1) * int(side2))
+
+#Area of a Triangle
 elif(shape == "triangle"):
     base = input("What is the base length of your triangle? ")
     height = input("What is the height of your triangle? ")
     print("The area of your triangle is: ",end='')
-    print(int(base)*int(height)/2)
+    print(int(base) * int(height)/2)
+
+#Area of a Circle(Approximate)
 elif(shape == "circle"):
     radius = input("What is the radius of your circle? ")
     print("The area of your circle is approximately: ",end='')
-    print(int(radius)*int(radius)*3.14159)
+    print(int(radius)**2 * math.pi)
+
+#Else
 else:
     print("This program does not run that shape")
