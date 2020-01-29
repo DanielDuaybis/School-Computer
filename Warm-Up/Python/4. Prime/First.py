62fc55bb798866b16637abc758aefc5df2d69baf
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2020-01-28 22:02:10 -0800

    Homework

--- Warm-Up/Python/4. Prime/First.py
+++ Warm-Up/Python/4. Prime/First.py
@@ -1,8 +1,14 @@
 #Warm-Up 1/27/20
 
-for x in range(1,1000000000000000000000000000000000000 + 1):
-       for i in range(2,x):
-              if(x % i) == 0:
-                     break
-              else:
-                     print(x)
+import math *
+
+count = 3
+one = 1
+while one == 1:
+    for x in range(2, int(math.sqrt(count) + 1)):
+        if count % x == 0: 
+            continue
+        if count % x != 0:
+            print count
+
+    count += 1
