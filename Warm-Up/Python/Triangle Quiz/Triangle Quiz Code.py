df2f4d498a88dea950d8972a01730bc71dee5e31
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2020-02-18 19:34:48 -0800

    Weekend Work

--- Warm-Up/Python/Triangle Quiz/Triangle Quiz Code.py
+++ Warm-Up/Python/Triangle Quiz/Triangle Quiz Code.py
@@ -12,14 +12,12 @@ erase = ['testvalue']
 erase.pop(0)
 shape = 0
 counter = 0
-erase2 = ['testvalue']
-erase2.pop(0)
 
 #Master while loop
 while True:
     
     #Check to see which shape to draw
-    if(shape % 2 == 0):
+    if(shape % 8 == 0):
         
     #Drawing Triangles
         for i in range(0,randint(25,50)):
@@ -48,7 +46,7 @@ while True:
         time.sleep(.5)
 
     #Check to see what shape to draw
-    elif(shape % 2 == 1):
+    elif(shape % 8 == 1):
         
         #Drawing Rectangles
         for i in range(0,randint(25,50)):
@@ -58,19 +56,193 @@ while True:
             rectangle.setOutline(color)
             rectangle.draw(w)
             time.sleep(.2)
-            erase2.append(rectangle)
+            erase.append(rectangle)
 
     #Reversing list
-        erase2.reverse()
+        erase.reverse()
 
     #Undrawing rectangles
-        for x in erase2:
+        for x in erase:
             x.undraw()
             time.sleep(.1)
 
     #Resetting erase list
-        for x in erase2:
-            erase2.pop(counter)
+        for x in erase:
+            erase.pop(counter)
+            counter = counter + 1
+    
+        counter = 0
+        time.sleep(.5)
+        
+    #Check to see which shape to draw
+    if(shape % 8 == 2):
+        
+    #Drawing Pentagons
+        for i in range(0,randint(25,50)):
+            pentagon = Polygon(Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)))
+            color = color_rgb(randint(0,255),randint(0,255),randint(0,255))
+            pentagon.setFill(color)
+            pentagon.setOutline(color)
+            pentagon.draw(w)
+            time.sleep(.2)
+            erase.append(pentagon)
+
+    #Reversing list
+        erase.reverse()
+
+    #Undrawing pentagons
+        for x in erase:
+            x.undraw()
+            time.sleep(.1)
+
+    #Resetting erase list
+        for x in erase:
+            erase.pop(counter)
+            counter = counter + 1
+    
+        counter = 0
+        time.sleep(.5)    
+        
+    #Check to see which shape to draw
+    if(shape % 8 == 3):
+        
+    #Drawing Hexagons
+        for i in range(0,randint(25,50)):
+            hexagon = Polygon(Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)))
+            color = color_rgb(randint(0,255),randint(0,255),randint(0,255))
+            hexagon.setFill(color)
+            hexagon.setOutline(color)
+            hexagon.draw(w)
+            time.sleep(.2)
+            erase.append(hexagon)
+
+    #Reversing list
+        erase.reverse()
+
+    #Undrawing hexagons
+        for x in erase:
+            x.undraw()
+            time.sleep(.1)
+
+    #Resetting erase list
+        for x in erase:
+            erase.pop(counter)
+            counter = counter + 1
+    
+        counter = 0
+        time.sleep(.5)
+
+    #Check to see which shape to draw
+    if(shape % 8 == 4):
+        
+    #Drawing Heptagons
+        for i in range(0,randint(25,50)):
+            heptagon = Polygon(Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)))
+            color = color_rgb(randint(0,255),randint(0,255),randint(0,255))
+            heptagon.setFill(color)
+            heptagon.setOutline(color)
+            heptagon.draw(w)
+            time.sleep(.2)
+            erase.append(heptagon)
+
+    #Reversing list
+        erase.reverse()
+
+    #Undrawing heptagons
+        for x in erase:
+            x.undraw()
+            time.sleep(.1)
+
+    #Resetting erase list
+        for x in erase:
+            erase.pop(counter)
+            counter = counter + 1
+    
+        counter = 0
+        time.sleep(.5)
+        
+    #Check to see which shape to draw
+    if(shape % 8 == 5):
+        
+    #Drawing Octagons
+        for i in range(0,randint(25,50)):
+            octagon = Polygon(Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)))
+            color = color_rgb(randint(0,255),randint(0,255),randint(0,255))
+            octagon.setFill(color)
+            octagon.setOutline(color)
+            octagon.draw(w)
+            time.sleep(.2)
+            erase.append(octagon)
+
+    #Reversing list
+        erase.reverse()
+
+    #Undrawing octagons
+        for x in erase:
+            x.undraw()
+            time.sleep(.1)
+
+    #Resetting erase list
+        for x in erase:
+            erase.pop(counter)
+            counter = counter + 1
+    
+        counter = 0
+        time.sleep(.5)
+        
+    #Check to see which shape to draw
+    if(shape % 8 == 6):
+        
+    #Drawing nonagons
+        for i in range(0,randint(25,50)):
+            nonagon = Polygon(Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)))
+            color = color_rgb(randint(0,255),randint(0,255),randint(0,255))
+            nonagon.setFill(color)
+            nonagon.setOutline(color)
+            nonagon.draw(w)
+            time.sleep(.2)
+            erase.append(nonagon)
+
+    #Reversing list
+        erase.reverse()
+
+    #Undrawing nonagons
+        for x in erase:
+            x.undraw()
+            time.sleep(.1)
+
+    #Resetting erase list
+        for x in erase:
+            erase.pop(counter)
+            counter = counter + 1
+    
+        counter = 0
+        time.sleep(.5)
+        
+    #Check to see which shape to draw
+    if(shape % 8 == 7):
+        
+    #Drawing decagons
+        for i in range(0,randint(25,50)):
+            decagon = Polygon(Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)),Point(randint(0,600),randint(0,600)))
+            color = color_rgb(randint(0,255),randint(0,255),randint(0,255))
+            decagon.setFill(color)
+            decagon.setOutline(color)
+            decagon.draw(w)
+            time.sleep(.2)
+            erase.append(decagon)
+
+    #Reversing list
+        erase.reverse()
+
+    #Undrawing decagons
+        for x in erase:
+            x.undraw()
+            time.sleep(.1)
+
+    #Resetting erase list
+        for x in erase:
+            erase.pop(counter)
             counter = counter + 1
     
         counter = 0
