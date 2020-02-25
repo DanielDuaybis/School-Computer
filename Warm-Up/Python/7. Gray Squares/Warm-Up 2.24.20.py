ba7bc0725d57f765dad542963db5b5504af179fc
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2020-02-25 11:20:44 -0800

    Day's Work

--- Warm-Up/Python/7. Gray Squares/Warm-Up 2.24.20.py
+++ Warm-Up/Python/7. Gray Squares/Warm-Up 2.24.20.py
@@ -2,12 +2,17 @@
 from graphics import *
 from random import *
 
+#General Creations
 w = GraphWin('Warm-Up',500,500)
 w.setBackground('white')
 squares = []
+win = Text(Point(250,250),'YOU DID IT')
+win.setFill('cyan')
+win.setSize(36)
+win.setStyle('bold italic')
 
 #Creating Squares
-for x in range(1,600):
+for x in range(1,700):
     p1x = randint(0,500)
     p1y = randint(0,500)
     p2x = p1x + 20
@@ -18,19 +23,40 @@ for x in range(1,600):
     square.setFill(randgray)
     square.draw(w)
     squares.append(square)
-    time.sleep(.008)
+    time.sleep(.004)
 
 #Making Blue Square
 squares[589].setFill('cyan')
 
 #Moving Squares
-m = w.getMouse()
-p1 = squares[589].getP1()
-p2 = squares[589].getP2()
-if(m.getX() < p1.getX() and m.getX() > p2.getX() and m.getX() > p1.getY() and m.getX() < p2.getY()):
-    while True:
+while True:
+    
+    #Setting Coordinate Variables
+    m = w.getMouse()
+    mx = m.getX()
+    my = m.getY()
+    p1 = squares[589].getP1()
+    p1x = p1.getX()
+    p1y = p1.getY()
+    p2 = squares[589].getP2()
+    p2x = p2.getX()
+    p2y = p2.getY()
+    
+    #Button and Moving Squares
+    if(mx >= p1x and mx <= p2x and my >= p1y and my <= p2y):
         for i in squares:
-            i.move(randint(-5,5),randint(-5,5))
+            i.move(randint(-10,10),randint(-10,10))
             rgb = randint(0,255)
             randgray = color_rgb(rgb,rgb,rgb)
-        i.setFill(randgray)
+            i.setFill(randgray)
+        win.draw(w)
+        while True:
+            for i in squares:
+                i.move(randint(-10,10),randint(-10,10))
+                rgb = randint(0,255)
+                randgray = color_rgb(rgb,rgb,rgb)
+                i.setFill(randgray)
+                
+    #Moving Blue square
+    else:
+        squares[589].move(randint(-30,30),randint(-30,30))
