222f527353c3108c3dd01e3565f2b0f159af7c9f
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2020-03-05 11:14:28 -0800

    Day's Work

--- Python/Python Labs/36. guess word/Lab 36.py
+++ Python/Python Labs/36. guess word/Lab 36.py
@@ -5,9 +5,17 @@ from random import *
 file = open('ColorList.txt','r')
 colors = file.readlines()
 
+
+
 while True:
     letters = []
     color = colors[randint(0,8)]
+
+    listToStr = "".join([str(elem) for elem in color])
+    if(int(len(listToStr)) > 3):
+        listToStr = listToStr[0:len(listToStr) - 1]
+
+    print("/",listToStr,"/")
 
     for i in color:
         letters.append(i)
@@ -21,6 +29,26 @@ while True:
         letter = letters[number]
         print(letter,end='')
         letters.pop(number)
-    
+
+    print()
+    guess = input("What color is this? ")
+    if(guess == str(color)):
+        print()
+        print("Congratulations!")
+        again = input("Would you like to play again? (yese or no) ")
+        if(again == "no"):
+            break;
+    else:
+        while True:
+            print()
+            print("YOU ARE WRONG!")
+            guess = input("Please try again: ")
+            if(guess == color):
+                print()
+                print("Congratulations!")
+                again = input("Would you like to play again? (yese or no) ")
+                if(again == "no"):
+                    break;
+
 print()
 print("Thanks for playing!")
