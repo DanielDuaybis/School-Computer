eb34d4ad3a7b42bb9fb437279dd667b2589a8406
DanielDuaybis <56407980+DanielDuaybis@users.noreply.github.com>
2020-03-11 11:20:18 -0700

    Day's Work

---
+++ Unity/First Test/Assets/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forForce = 2000f;
+    public float sideForce = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forForce * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, forForce * Time.deltaTime *  -1);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(forForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(-forForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
